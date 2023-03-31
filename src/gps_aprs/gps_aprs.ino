3fa00122f2b57080b3668548334c025ec67a6a9e
Hasith Perera <12579141+hasithperera@users.noreply.github.com>
2023-03-31 15:30:28 -0400

    radio mods

--- src/gps_aprs/gps_aprs.ino
+++ src/gps_aprs/gps_aprs.ino
@@ -1,8 +1,12 @@
 #include <SoftwareSerial.h>
 
+#define RX_radio 14
+#define TX_radio 15
 //#include <LibAPRS.h>
+SoftwareSerial mySerial(RX_radio, TX_radio); // RX, TX
 
 #define led 13
+String a;
 
 // the setup routine runs once when you press reset:
 void setup() {                
@@ -12,11 +16,41 @@ void setup() {
   Serial.begin(9600);
   Serial.println("WVU Rocketry - GPS APRS tracker");
 
+  init_radio();
+  radio_on();
+  radio_RX();
+  radio_low_pw();
+
+
+  mySerial.begin(9600);
+  mySerial.print("\r\n");
+
+  mySerial.print("AT+DMOCONNECT\r\n");
+  a = mySerial.readStringUntil('\n'); 
+  mySerial.print("AT+DMOCONNECT\r\n");
+  a = mySerial.readStringUntil('\n'); 
+
+  Serial.print(">>");
+  Serial.println(a);
+
+  mySerial.print("AT+DMOSETGROUP=1,144.3900,144.3900,0000,5,0000\r\n");
+  radio_TX();
 }
 
 void loop() {
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);               // wait for a second
+
+  if (mySerial.available()>0){
+     //a = mySerial.readString('\n');
+     Serial.print(">");
+     //Serial.print(a);
+  }
+
+ // radio_TX();
+ // delay(2000);
+ // radio_RX();
+ // delay(2000);
+
+  // blank line to separate data from the two ports:
+
+      // wait for a second
 }
--- src/gps_aprs/radio.ino
+++ src/gps_aprs/radio.ino
@@ -10,14 +10,50 @@
 // wake A6(D20)
 
 #define radio_wake 20
+#define radio_ppt 17
+#define radio_pwr 21
 
 
 void init_radio(){
   
+  // 
   pinMode(radio_wake,OUTPUT);
   digitalWrite(radio_wake,LOW); //sleep the radio at init
+
+  // PPT  0: TX
+  //      1: RX
+  // board : (A3 missing pin)
+  // set this pin to RX
+  pinMode(radio_ppt,OUTPUT);
+  digitalWrite(radio_ppt,HIGH);
+
+  pinMode(radio_pwr,OUTPUT);
+  
+
+
+}
+
+void radio_TX(){
+  digitalWrite(radio_ppt,LOW);
+}
+
+void radio_RX(){
+  digitalWrite(radio_ppt,HIGH);
 }
 
 void radio_on(){
+  digitalWrite(radio_wake,HIGH); 
+}
+
+void radio_off(){
   digitalWrite(radio_wake,LOW); 
+}
+
+void radio_high_pw(){
+  pinMode(radio_pwr,INPUT);
+}
+
+void radio_low_pw(){
+    pinMode(radio_pwr,OUTPUT);
+    digitalWrite(radio_pwr,LOW);
 }
